77e07ec40990d233a053272afc3ddd0e6e90c3c8
Rushikesh <rushikeshbaraskar1@gmail.com>
2024-07-17 00:02:45 +0530

    project created

---
+++ src/test/java/com/system/management/bank/BankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.system.management.bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
