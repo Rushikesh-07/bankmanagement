9e553ef15569e49ffeb1e3159cdc8fb39bfdb131
Rushikesh <rushikeshbaraskar1@gmail.com>
2024-07-20 19:41:47 +0530

    basic contoller is created to run application

---
+++ src/main/java/com/system/management/bank/controller/CustomerController.java
@@ -0,0 +1,14 @@
+package com.system.management.bank.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CustomerController {
+	
+	@GetMapping("/userhome")
+	public String userHome()
+	{
+		return "Welcome to user home controller";
+	}
+}
