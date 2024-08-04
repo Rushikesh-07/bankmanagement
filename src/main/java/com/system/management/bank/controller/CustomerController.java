2b498ef5d4ae967ef52d59402516ade600bc7e7c
Rushikesh <rushikeshbaraskar1@gmail.com>
2024-08-04 23:38:56 +0530

    CRU operation methods are written

--- src/main/java/com/system/management/bank/controller/CustomerController.java
+++ src/main/java/com/system/management/bank/controller/CustomerController.java
@@ -1,12 +1,26 @@
 package com.system.management.bank.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.system.management.bank.entity.Customer;
+import com.system.management.bank.service.CustomerService;
 
 @RestController
 @RequestMapping("/customers")
 public class CustomerController {
+	
+	@Autowired
+    private CustomerService customerService;
 	
 	@GetMapping("/userhome")
 	public String userHome()
@@ -14,5 +28,21 @@ public class CustomerController {
 		return "Welcome to user home controller";
 	}
 	
+	@PostMapping("/new-save")
+    public ResponseEntity<Customer> createCustomer(@RequestBody Customer customer) {
+        Customer savedCustomer = customerService.saveCustomer(customer);
+        return ResponseEntity.ok(savedCustomer);
+    }
 	
+	@GetMapping("/get-all")
+    public ResponseEntity<List<Customer>> getAllCustomers() {
+        List<Customer> customers = customerService.getAllCustomers();
+        return ResponseEntity.ok(customers);
+    }
+	
+	@PutMapping("/update-customer/{id}")
+    public ResponseEntity<Customer> updateCustomer(@PathVariable Long id, @RequestBody Customer customer) {
+        Customer updatedCustomer = customerService.updateCustomer(id, customer);
+        return updatedCustomer != null ? ResponseEntity.ok(updatedCustomer) : ResponseEntity.notFound().build();
+    }
 }
--- src/main/java/com/system/management/bank/service/CustomerService.java
+++ src/main/java/com/system/management/bank/service/CustomerService.java
@@ -1,5 +1,7 @@
 package com.system.management.bank.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +16,22 @@ public class CustomerService {
 
     public Customer saveCustomer(Customer customer) {
         return customerRepository.save(customer);
+    }
+
+    public List<Customer> getAllCustomers() {
+        return customerRepository.findAll();
+    }
+    
+    public Customer getCustomerById(Long id) {
+        return customerRepository.findById(id).orElse(null);
+    }
+    
+    public Customer updateCustomer(Long id, Customer customer) {
+        if (customerRepository.existsById(id)) {
+            customer.setCustid(id);
+            return customerRepository.save(customer);
+        }
+        return null;
     }
 	
 }
